b26e382b38de1fba422e959ae95b8425b9b6250f
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2022-03-23 00:18:23 +0000

    Tweaks server status listing

--- components/HubServer.vue
+++ components/HubServer.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="hub-server">
-		<div v-dompurify-html="status" class="hub-server__status"></div>
+		<div v-dompurify-html="newStatus" class="hub-server__status"></div>
 		<div class="ml-4 text-center">
 			<a :href="`byond://BYOND.world.${urlId}`" class="cta cta--sm px-10">
 				Join
@@ -39,7 +39,41 @@ export default {
 		},
 	},
 
+	beforeMount() {
+		// Handle adding brackets to server text
+		const statusSplit = this.status.split("<br>");
+		let addPrefixBracket = false;
+		let addSuffixBracket = false;
+
+		if(statusSplit.length > 0) {
+			// Check first line
+			const leftFirstCount = statusSplit[0].split("[").length - 1;
+			const rightFirstCount = statusSplit[0].split("]").length - 1;
+			if(rightFirstCount > leftFirstCount) {
+				addPrefixBracket = true;
+			}
+
+			// Check last line
+			const leftLastCount = statusSplit[statusSplit.length - 1].split("[").length - 1;
+			const rightLastCount = statusSplit[statusSplit.length - 1].split("]").length - 1;
+			if(leftLastCount > rightLastCount) {
+				addSuffixBracket = true;
+			}
+		}
+
+		this.newStatus = this.status;
+
+		// Actually add it
+		if(addPrefixBracket) {
+			this.newStatus = "[" + this.newStatus;
+		}
+		if(addSuffixBracket) {
+			this.newStatus = this.newStatus + "]";
+		}
+	},
+
 	mounted() {
+		// Handle URLs
 		const links = this.$el.querySelectorAll('a')
 		for (const link of links) {
 			link.setAttribute('target', '_blank')
